8b2b43c6fba642c02dd2d5e9b7852f319b330259
alswo444 <alswo444@naver.com>
2020-08-31 07:10:08 +0900

    header update

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,15 @@
 $(document).ready(() => {
   $("#header").load("../../pages/header.html");
 })
+
+$(document).ready(function(){ 
+  $(".menubar").click(function(){ 
+    if($(".nav").is(":visible")){ 
+      $(".nav").css("display", "none"); 
+    }else{ 
+      $(".nav").css("display", "block"); 
+    } 
+  }); 
+});
+
+
