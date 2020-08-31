d17f54ef3b8652a703e9b984a2d8fcd46b2bb0d1
alswo444 <alswo444@naver.com>
2020-09-01 08:56:10 +0900

    update

--- src/js/header.js
+++ src/js/header.js
@@ -2,14 +2,10 @@ $(document).ready(() => {
   $("#header").load("../../pages/header.html");
 })
 
-$(document).ready(function(){ 
-  $(".menubar").click(function(){ 
-    if($(".nav").is(":visible")){ 
-      $(".nav").css("display", "none"); 
-    }else{ 
-      $(".nav").css("display", "block"); 
-    } 
-  }); 
+$(document).ready(function(){
+  $("#menubar").click(function(){
+    $(this).next(".nav").toggleClass("ul");
+  });
 });
 
 
