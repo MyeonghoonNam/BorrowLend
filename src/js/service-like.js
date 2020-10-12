7005ccae3cfc483477b16789b339667a72b88de2
MyeongHoonNam <codenmh0822@naver.com>
2020-10-13 01:14:53 +0900

    푸터 하단 고정 설정 밑 관심목록 구현

---
+++ src/js/service-like.js
@@ -0,0 +1,25 @@
+// 동적 상품리스트 레이아웃 구현
+$(document).ready(function(){
+  var ProductList = [];
+  ProductList = document.getElementsByClassName("servicelike-product-element");
+  
+  if(ProductList.length%3 == 2){
+    
+    var storebox = document.getElementsByClassName("servicelike-product")[0];
+    var layoutbox = document.createElement('div');
+    
+    layoutbox.setAttribute("class", "servicelike-product-element");
+    layoutbox.setAttribute("style", "display:block");
+    storebox.appendChild(layoutbox);
+    ProductList[ProductList.length-1].style.border="none";
+    ProductList[ProductList.length-1].style.cursor="default";
+  }
+});
+
+function ProductClick(element){
+  var product = $(element).find(".element_btn")
+  
+  console.log(product);
+
+  product[0].click();
+}
